d8b4b15554942e9808199977fbabb60cd6aca5dc
Jonas Damtoft <jonasdamtoft@hotmail.com>
2017-04-20 15:12:00 +0200

    Pull from https://github.com/fish-shell/fish-shell/share/completions/gradle.fish

--- completions/gradle.fish
+++ completions/gradle.fish
@@ -1,5 +1,44 @@
-#inspired by https://github.com/zmalltalker/fish-nuggets/blob/master/completions/rake.fish
+# gradle is a build system.
+# See: https://gradle.org
 
+complete -c gradle -l help -s h -d 'Show help'
+complete -c gradle -l no-rebuild -s a -d 'Don\'t rebuild dependencies'
+complete -c gradle -l build-file -s b -r -d 'Specify build file'
+complete -c gradle -l settings-file -s c -r -d 'Specify settings file'
+complete -c gradle -l configure-on-demand -d 'Only relevent project are configured'
+complete -c gradle -l console -x -d 'Specify console output type' -a 'plan auto rich'
+complete -c gradle -l continue -d 'Continue task execution after failure'
+complete -c gradle -l system-prop -s D -r -d 'Set system property of the JVM'
+complete -c gradle -l debug -s d -d 'Log in debug mode'
+complete -c gradle -l daemon -d 'Uses Gradle Daemon to run build'
+complete -c gradle -l forground -d 'Uses Gradle Daemon in forground'
+complete -c gradle -l gradle-user-home -s g -r -d 'Specify gradle user home directory'
+complete -c gradle -l gui -d 'Launch Gradle GUI'
+complete -c gradle -l initscript -s I -r -d 'Specify an initialization script'
+complete -c gradle -l info -s i -d 'Set log level to info'
+complete -c gradle -l include-build -r -d 'Include specified build in composite'
+complete -c gradle -l dry-run -s m -d 'Runs build with all task actions disabled'
+complete -c gradle -l max-workers -x -d 'Configure number of concurrent workers' -a '1\t 2\t 3\t 4\t 5\t 6\t 7\t 8\t 9\t 10\t'
+complete -c gradle -l no-daemon -d 'Don\'t use deamon'
+complete -c gradle -l offline -d 'Don\'t use network resources'
+complete -c gradle -l project-prop -s P -x -d 'Set project property for build script'
+complete -c gradle -l project-dir -s p -r -d 'Specify start directory'
+complete -c gradle -l parallel -d 'Build project in parallel'
+complete -c gradle -l profile -d 'Profile build execution time'
+complete -c gradle -l project-cache-dir -r -d 'Specify project cache directory'
+complete -c gradle -l quiet -s q -d 'Only log erros'
+complete -c gradle -l recompile-scripts -d 'Force build script recompiling'
+complete -c gradle -l refresh-dependencies -d 'Refresh state of dependencies'
+complete -c gradle -l rerun-tasks -d 'Ignore previously cached dependencies'
+complete -c gradle -l full-stacktrace -s S -d 'Print out full stacktrace for all exceptions'
+complete -c gradle -l status -d 'Shows status of running andrecently stopped daemon'
+complete -c gradle -l stop -d 'Stop daemon if running'
+complete -c gradle -l continuous -s t -d 'Enable continuous build'
+complete -c gradle -l no-search-upward -s u -d 'Don\'t search in parent folders for settings file'
+complete -c gradle -l version -s v -d 'Print version'
+complete -c gradle -l exclude-task -s x -x -d 'Specify task to be excluded from execution'
+
+# https://github.com/hanny24/gradle-fish/blob/master/gradle.load
 function __cache_or_get_gradle_completion
   mkdir -p "/tmp/gradle_completion_cache_for_$USER"
   set -l hashed_pwd (echo $PWD | md5sum | awk '{ print $1 }')
@@ -19,39 +58,3 @@ function __run_gradle_completion
 end
 
 complete -x -c gradle -a "(__cache_or_get_gradle_completion)" -n '__run_gradle_completion'
-
-complete -c gradle -s 'a' -l 'no-rebuild' -d "Do not rebuild project dependencies."
-complete -c gradle -s 'b' -l 'build-file' -d "Specifies the build file."
-complete -c gradle -s 'c' -l 'settings-file' -d "Specifies the settings file."
-complete -c gradle -l 'configure-on-demand' -d "Only relevant projects are configured in this build run. This means faster build for large multi-project builds. [incubating]"
-complete -c gradle -l 'console' -d "Specifies which type of console output to generate. Values are 'plain', 'auto' (default) or 'rich'."
-complete -c gradle -l 'continue' -d "Continues task execution after a task failure."
-complete -c gradle -s 'D' -l 'system-prop' -d "Set system property of the JVM (e.g. -Dmyprop=myvalue)."
-complete -c gradle -s 'd' -l 'debug' -d "Log in debug mode (includes normal stacktrace)."
-complete -c gradle -l 'daemon' -d "Uses the Gradle daemon to run the build. Starts the daemon if not running."
-complete -c gradle -l 'foreground' -d "Starts the Gradle daemon in the foreground. [incubating]"
-complete -c gradle -s 'g' -l 'gradle-user-home' -d "Specifies the gradle user home directory."
-complete -c gradle -l 'gui' -d "Launches the Gradle GUI."
-complete -c gradle -s 'I' -l 'init-script' -d "Specifies an initialization script."
-complete -c gradle -s 'i' -l 'info' -d "Set log level to info."
-complete -c gradle -s 'm' -l 'dry-run' -d "Runs the builds with all task actions disabled."
-complete -c gradle -l 'max-workers' -d "Configure the number of concurrent workers Gradle is allowed to use. [incubating]"
-complete -c gradle -l 'no-color' -d "Do not use color in the console output. [deprecated - use --console=plain instead]"
-complete -c gradle -l 'no-daemon' -d "Do not use the Gradle daemon to run the build."
-complete -c gradle -l 'offline' -d "The build should operate without accessing network resources."
-complete -c gradle -s 'P' -l 'project-prop' -d "Set project property for the build script (e.g. -Pmyprop=myvalue)."
-complete -c gradle -s 'p' -l 'project-dir' -d "Specifies the start directory for Gradle. Defaults to current directory."
-complete -c gradle -l 'parallel' -d "Build projects in parallel. Gradle will attempt to determine the optimal number of executor threads to use. [incubating]"
-complete -c gradle -l 'parallel-threads' -d "Build projects in parallel, using the specified number of executor threads. [deprecated - Please use --parallel, optionally in conjunction with --max-workers.] [incubating]"
-complete -c gradle -l 'profile' -d "Profiles build execution time and generates a report in the <build_dir>/reports/profile directory."
-complete -c gradle -l 'project-cache-dir' -d "Specifies the project-specific cache directory. Defaults to .gradle in the root project directory."
-complete -c gradle -s 'q' -l 'quiet' -d "Log errors only."
-complete -c gradle -l 'recompile-scripts' -d "Force build script recompiling."
-complete -c gradle -l 'refresh-dependencies' -d "Refresh the state of dependencies."
-complete -c gradle -l 'rerun-tasks' -d "Ignore previously cached task results."
-complete -c gradle -s 'S' -l 'full-stacktrace' -d "Print out the full (very verbose) stacktrace for all exceptions."
-complete -c gradle -s 's' -l 'stacktrace' -d "Print out the stacktrace for all exceptions."
-complete -c gradle -l 'stop' -d "Stops the Gradle daemon if it is running."
-complete -c gradle -s 'u' -l 'no-search-upward' -d "Don't search in parent folders for a settings.gradle file."
-complete -c gradle -s 'v' -l 'version' -d "Print version info."
-complete -c gradle -s 'x' -l 'exclude-task' -d "Specify a task to be excluded from execution."
